8705722adda4d0bd84972b50264d7d8df2fa82de
Young Ferreira <youngferreira1@gmail.com>
2026-03-23 19:32:05 -0300

    Add files via upload

---
+++ site/app.js
@@ -0,0 +1,8 @@
+function mBarraLateral(){
+     const BarraLateral = document.querySelector('.BarraLateral');
+     BarraLateral.style.display = 'flex';
+}
+function eBarraLateral(){
+    const BarraLateral = document.querySelector('.BarraLateral')
+    BarraLateral.style.display = 'none'
+}
